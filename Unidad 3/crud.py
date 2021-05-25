b45cd913bfa37ef0c08dfbf02edd00c3ca9a4441
iam3mer <jhstbarrera@utp.edu.co>
2021-05-25 13:16:37 -0500

    Crud, Update

--- Unidad 3/crud.py
+++ Unidad 3/crud.py
@@ -24,22 +24,36 @@ dbAnimales = {
     }
 }
 
+vertebrados = ['mamifero', 'ave', 'pez', 'reptil', 'anfibio']
+invertebrados = ['porifero', 'cnidario', 'molusco', 'anelido', 'artropodo']
+alimentacion = ['herbivoro', 'carnivoro', 'omnivoro']
+reproduccion = ['oviparo', 'viviparo', 'ovoviviparo']
 
-def comprobacion(msj:str, lista:list)->str:
+
+def comprobacion(msj:str, lista:list, tipo:str)->str:
     while True:
-        entra = input(msj)
+        if tipo == 'int':
+            entra = input(msj)
+            if entra.isdigit():
+                entra = int(entra)
+        elif tipo == 'str':
+            entra = input(msj)
         for item in lista:
             if entra == item:
                 return entra
 
+def comprobacionSN(msj:str):
+    while True:
+        entrada = input(msj)
+        if entrada in ['S', 's', 'si', 'SI']:
+            return True
+        elif entrada in ['N', 'n', 'no', 'NO']:
+            return False
 
+
+# CREAD
 def crear(db:dict={})->dict:
 
-    vertebrados = ['mamifero', 'ave', 'pez', 'reptil', 'anfibio']
-    invertebrados = ['porifero', 'cnidario', 'molusco', 'anelido', 'artropodo']
-    alimentacion = ['herbivoro', 'carnivoro', 'omnivoro']
-    reproduccion = ['oviparo', 'viviparo', 'ovoviviparo']
-    
     print('Esta a pundo de añadir un animal')
 
     animal = input('Ingrese el nombre del animal: ')
@@ -54,20 +68,20 @@ def crear(db:dict={})->dict:
         msj = 'Ingrese la estructura del del animal de los valores presentados: '
         if estructura == '1':
             print(vertebrados)
-            tipo = comprobacion(msj, vertebrados)
+            tipo = comprobacion(msj, vertebrados, 'str')
             break
         elif estructura == '2':
             print(invertebrados)
-            tipo = comprobacion(msj, invertebrados)
+            tipo = comprobacion(msj, invertebrados, 'str')
             break
 
     print(alimentacion)
     msj = 'Ingrese el tipo de alimentacion de los valores presentados: '
-    seleccionAlimentacion = comprobacion(msj, alimentacion)
+    seleccionAlimentacion = comprobacion(msj, alimentacion, 'str')
 
     msj = 'Ingrese el tipo de reproduccion de los valores presentados: '
     print(reproduccion)
-    seleccionReproduccion = comprobacion(msj, reproduccion)
+    seleccionReproduccion = comprobacion(msj, reproduccion, 'str')
 
     if estructura == '1':
         estructuraAnimal = {
@@ -96,10 +110,14 @@ def crear(db:dict={})->dict:
     return db
 
 
+# READ
 def consultar(db:dict={})->None:
 
+    keys = list(db.keys())
+
     print('Consulta de un animal x')
-    llaveAnimal = int(input('Ingrese un numero entero: '))
+    msj = 'Ingrese un numero entero: '
+    llaveAnimal = comprobacion(msj, keys, 'int')
 
     animalConsultado = db[llaveAnimal]
 
@@ -114,4 +132,38 @@ def consultar(db:dict={})->None:
     print(f'Modo de reproducción: {animalConsultado["reproduccion"]}')
 
 
+# UPDATE
+def actualizar(db:dict)->dict:
+
+    keys = list(db.keys())
+
+    print('Actualizar un animal x')
+    msj = 'Ingrese un numero entero: '
+    llaveAnimal = comprobacion(msj, keys, 'int')
+
+    msj = 'Desea modificar el nombre del animal [S/s] Sí [N/n] No '
+    if comprobacionSN(msj):
+        animal = input('Ingrese el nuevo nombre: ')
+        db[llaveAnimal]['animal'] = animal
+
+    msj = 'Desea modificar la alimentación del animal? [S/s] Sí [N/n] No '
+    if comprobacionSN(msj):
+        print(alimentacion)
+        msj = 'Ingrese la alimentación: '
+        alimentacion = comprobacion(msj, alimentacion, 'str')
+        db[llaveAnimal]['alimentacion'] = alimentacion
+
+    reproduccion = input('Desea modificar la reproduccion del animal? [S/s] Sí [N/n] No ')
+    if reproduccion in ['S', 's', 'si', 'SI']:
+        print(reproduccion)
+        msj = 'Ingrese la reproducción: '
+        reproduccion = comprobacion(msj, reproduccion, 'str')
+        db[llaveAnimal]['reproduccion'] = reproduccion
+
+    return db
+
+#dbActualizada = crear(dbAnimales)
+#consultar(dbActualizada)
+
 consultar(dbAnimales)
+dbActualiza = actualizar(dbAnimales)
