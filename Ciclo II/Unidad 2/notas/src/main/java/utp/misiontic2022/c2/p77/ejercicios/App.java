ea1d11f7abfc5a0084b30065ee6233274fadabc3
iam3mer <jhstbarrera@utp.edu.co>
2021-07-07 07:51:24 -0500

    notas v2

--- Ciclo II/Unidad 2/notas/src/main/java/utp/misiontic2022/c2/p77/ejercicios/App.java
+++ Ciclo II/Unidad 2/notas/src/main/java/utp/misiontic2022/c2/p77/ejercicios/App.java
@@ -9,23 +9,18 @@ public class App
     public static void main( String[] args )
     {
         Double auxNota = Double.parseDouble(args[1]);
-        Double promedio = 0.0;
-        Integer indNota = 1;
+        Double promedio = auxNota;
 
         for (int i = 2; i < args.length; i++) {
+            promedio += Double.parseDouble(args[i]);
             if (Double.parseDouble(args[i]) < auxNota) {
                 auxNota = Double.parseDouble(args[i]);
-                indNota = i;
             }
         }
 
-        args[indNota] = "0";
-
-        for (int i = 1; i < args.length; i++) {
-            promedio += Double.parseDouble(args[i]);
-        }
-
+        promedio -= auxNota;
         promedio /= 4;
+        promedio *= 0.05;
 
         System.out.printf("El promedio del estudiantes con id %s es: %.2f",
                         args[0], promedio);
