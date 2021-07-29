5c90abd20f693b5371f7e7885b1a920a1b63d87e
iam3mer <jhstbarrera@utp.edu.co>
2021-07-29 07:18:25 -0500

    BookShop JDBC

---
+++ Ciclo II/Unidad 4/bookshop/src/main/java/utp/misiontic2022/c2/p77/unidad4/bookshop/App.java
@@ -0,0 +1,13 @@
+package utp.misiontic2022.c2.p77.unidad4.bookshop;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        new BookShop().start();
+    }
+}
---
+++ Ciclo II/Unidad 4/bookshop/src/test/java/utp/misiontic2022/c2/p77/unidad4/bookshop/AppTest.java
@@ -0,0 +1,20 @@
+package utp.misiontic2022.c2.p77.unidad4.bookshop;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
