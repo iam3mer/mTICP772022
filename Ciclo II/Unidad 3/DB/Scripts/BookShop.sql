75105519511b52ad5a6b19b909e1159ff7579e10
iam3mer <jhstbarrera@utp.edu.co>
2021-08-02 13:13:33 -0500

    Ejemplo MVC-JPA

--- Ciclo II/Unidad 3/DB/Scripts/BookShop.sql
+++ Ciclo II/Unidad 3/DB/Scripts/BookShop.sql
@@ -21,3 +21,15 @@ create table sales (
 
 -- Obtener libro por isbn
 select * from books where isbn = 'sdfsdf';
+
+-- Obtener lista de libros
+SELECT * FROM books;
+
+-- Obtener la cantidad de un libro en stock por su id
+SELECT amount FROM stock WHERE id_book = 1;
+
+-- Realizar venta
+INSERT INTO sales (sale_date,id_book,amount) VALUES (datetime('now'),1,2);
+
+-- Actualizar el stock
+UPDATE stock SET amount = 8 WHERE id_book = 1;
