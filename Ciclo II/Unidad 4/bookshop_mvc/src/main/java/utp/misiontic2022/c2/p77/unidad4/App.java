10d84f83f2af1893468d54dd30ec96a36138aa02
iam3mer <jhstbarrera@utp.edu.co>
2021-08-02 13:14:22 -0500

    Ejemplo MVC-JDBC

---
+++ Ciclo II/Unidad 4/bookshop_mvc/src/main/java/utp/misiontic2022/c2/p77/unidad4/App.java
@@ -0,0 +1,11 @@
+package utp.misiontic2022.c2.p77.unidad4;
+
+import utp.misiontic2022.c2.p77.unidad4.vista.Menu;
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+        Menu.iniciar();
+    }
+}
